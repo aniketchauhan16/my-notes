99ae77f57671e7654450b2e8d98b8d112e9a8325
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-04-10 05:39:57 +0530

    Add datatypes.js

---
+++ JS_FOUNDATION/JS_BASICS/datatypes.js
@@ -0,0 +1,22 @@
+console.log("hi aniket this side");
+
+let score = 71; // defining a value for variable
+newscore = score; // giving it to next variable
+console.log(newscore);
+
+let movie = "batman";
+movie = "superman";
+console.log(movie); // prints superman
+
+const player = "cristiano";
+//player = "messi";   // gives error as constant cant be changed
+console.log(player);
+
+let teatypes = ["lemon tea", "orange tea", "oolong tea"];
+let user = { user: "aniket", user2: "chauhan", user3: "hitesh" };
+console.log(movie);
+
+let isLoggedin = true;
+let isPaid = false;
+// console.log(isLoggedin && isPaid); #false
+console.log(isLoggedin || isPaid); //#true
