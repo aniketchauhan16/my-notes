0fa0fb129bad1f922fbd9eddfdf25fa9586a1c4b
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-04-10 05:39:57 +0530

    Add condition-challenge.js

---
+++ JS_FOUNDATION/JS_BASICS/condition-challenge.js
@@ -0,0 +1,52 @@
+let username = "AniketChauhan";
+let anotherusername = "AniketChauhan";
+
+if (username == anotherusername) {
+  console.log("Pick Another Username");
+} else {
+  console.log("Username Is Available");
+}
+
+let score = 44; //number
+if (typeof score === "number") {
+  console.log("its an number");
+} else {
+  console.log("its Not an number");
+}
+
+let newscore = "44"; //string
+if (typeof newscore === "number") {
+  console.log("its an number");
+} else {
+  console.log("its Not an number");
+}
+
+let oldscore = []; //string
+if (typeof oldscore === "object") {
+  console.log("its an object");
+} else {
+  console.log("its Not an object");
+}
+
+let isTeaReady = false;
+
+if (isTeaReady) {
+  console.log("tea is ready");
+} else {
+  console.log("tea is not ready");
+}
+
+let isLOggedin = true;
+
+if (isLOggedin) {
+  console.log(" ready");
+} else {
+  console.log("not ready");
+}
+
+let items = [];
+if (items.length === 0) {
+  console.log("Array is Empty");
+} else {
+  console.log("Array is not empty");
+}
